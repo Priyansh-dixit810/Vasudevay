a061f233094b2c4ddded5911e3e9a3210914977c
Priyansh Dixit <priyanshdixit@Priyanshs-MacBook-Air.local>
2026-03-17 20:48:49 +0530

    Fixed image error-2

--- src/components/sections/start.tsx
+++ src/components/sections/start.tsx
@@ -18,8 +18,8 @@ export default function Start() {
           <Image
             src={
               isDark
-                ? "/images/Dark_livingroom.png"
-                : "/images/Light_livingroom.png"
+                ? "/images/dark_livingroom.png"
+                : "/images/light_livingroom.png"
             }
             alt="Living-room"
             fill
